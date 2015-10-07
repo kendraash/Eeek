af491e3a6daeb7e01e0fb2a72291d54fb990d665
Kendra Ash & Lindsay Culver <student@epicodus.com>
2015-10-07 14:21:55 -0700

    add component to add a new category

--- app/router.js
+++ app/router.js
@@ -6,11 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-
   this.route('kids', {});
   this.route('adults', {});
   this.route('trickOrTreat', {});
 
+  this.route('activities', {});
 });
 
 export default Router;
