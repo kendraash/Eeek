53039d2bd0129e8507efa79fc9969f10e2267e50
rachael berecka & yelena nedelko <student@epicodus.com>
2015-10-07 16:41:23 -0700

    End of day merge

--- app/routes/kids.js
+++ app/routes/kids.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+        activities: this.store.findAll('activity'),
+        categories: this.store.findAll('category')
+      });
+    },
+  actions: {
+    save(params) {
+      var newActivity = this.store.createRecord('activity', params);
+      newActivity.save();
+      this.transitionTo('kids');
+    }
+  }
 });
