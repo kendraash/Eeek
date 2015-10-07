7189c12c2dae72f48c64c6cf566b23925cfe7859
rachael berecka & yelena nedelko <student@epicodus.com>
2015-10-07 10:30:15 -0700

    Add initial routes

---
+++ tests/unit/routes/trick-or-treat-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:trick-or-treat', 'Unit | Route | trick or treat', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
